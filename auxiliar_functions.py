da999c04b56b056f73756edcc1e24b30f562ee16
brmedeiros <br.medeiros@gmail.com>
2016-12-25 22:12:03 -0300

    allows sharp to connect to discord by adding sharp.py
    also adds auxiliar_functions.py

---
+++ auxiliar_functions.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def sp_print(*args): #print() ends with a space instead of \n
+    return(print(*args, end = ' '))
+
+if __name__ == '__main__':
+    main()
---
+++ sharp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import discord
+import asyncio
+import datetime as dt
+
+import sharp_login_info as info
+import auxiliar_functions as auxf
+
+def main():
+
+    client = discord.Client()
+    
+    @client.async_event # equivalent to @client.event \n @asyncio.courotine
+    def on_ready():
+        '''
+        this function represents the event 'being ready'...
+        it should not be called anywhere
+        it is defined in the discord module
+        '''
+        print('s.h.a.r.p. v.0.1\n------\nLogged in as {0}, id: {1}'.format(client.user, client.user.id))
+        auxf.sp_print('Server(s) joined:')
+        for server in client.servers:
+            auxf.sp_print('{0}'.format(server))
+        print('\n------')
+        
+    @client.async_event    
+    def on_message(message):
+        '''
+        this function represents a 'message event' in any of the channels...
+        it should not be called anywhere
+        it is defined in the discord module
+        '''
+        # print messages written in the channels to the terminal screen 
+        print('{:%d/%m/%y %H:%M} @{} {}: {}'
+              .format(dt.datetime.now(), message.channel, message.author.name, message.content))
+        
+        if message.content.startswith('!#'):
+            pass
+        
+    client.run(info.token)
+
+if __name__ == '__main__':
+    main()
